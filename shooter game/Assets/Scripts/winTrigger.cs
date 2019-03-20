d8ed7b405a62c14e4cc26c659ccaaeda8f5097aa
manouijpelaar <manou.ijpelaar@gmail.com>
2019-03-20 13:55:24 +0100

    unity game

---
+++ shooter game/Assets/Scripts/winTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class winTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject completeLevelScreen;
+
+    void OnTriggerEnter(Collider win)
+    {   // when player collides with winscene, level complete
+        if (win.gameObject.tag == "player")
+        {
+            win.gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+            completeLevelScreen.SetActive(true);
+            Debug.Log("You Win!");
+        }
+    }
+}
